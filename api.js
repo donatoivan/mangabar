95d00304946a291c3098e28d6f8987aae46f41d1
Ivan Donato <donato.ivan@gmail.com>
2019-06-19 10:27:36 +1000

    first commit

---
+++ api.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const axios = require('axios')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.use(express.json());
+app.use(express.static('public'));
+const port = 3000;
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/manga', (req, res, next) => {
+  // const ghibli = axios.get("https://ghibliapi.herokuapp.com/films");
+  const manga = axios.get('https://api.jikan.moe/v3/genre/manga/1/');
+
+  // ghibli
+  manga
+  .then((response) => {
+    return response
+  })
+  .then((data) => {
+    res.render('index', {
+      data: data.data.manga
+    })
+  })
+})
+
+app.get('/manga/:id', (req, res) => {
+  const { id } = req.params
+  const singleManga = axios.get(`https://api.jikan.moe/v3/manga/${id}`)
+  singleManga
+    .then((response) => {
+      return response
+    })
+    .then((data) => {
+      res.render('show', {
+        data: data.data
+      })
+    })
+})
+
+app.post('/search', (req, res) => {
+  const userSearchTerm = encodeURI(req.body.mangaSearch)
+  console.log(req.body.mangaSearch)
+  console.log(userSearchTerm)
+  const searchManga = axios.get(`https://api.jikan.moe/v3/search/manga?q=${userSearchTerm}&page=1`)
+  searchManga
+  .then((response) => {
+    return response
+  })
+  .then((data) => {
+    res.render('search', {
+      data: data.data.results
+    })
+  })
+})
+
+app.get('/manga/genres/:id', (req, res) => {
+  const { id } = req.params
+  const singleManga = axios.get(`https://api.jikan.moe/v3/genre/manga/${id}`)
+  singleManga
+    .then((response) => {
+      return response
+    })
+    .then((data) => {
+      res.render('index', {
+        data: data.data.manga
+      })
+    })
+})
+
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
---
+++ server.js
@@ -0,0 +1,50 @@
+// const fetch = require('node-fetch');
+
+
+// async function getAPI() {
+//   const response = await fetch('/api');
+//   const json = await response.json();
+
+  const ghibli = fetch("https://ghibliapi.herokuapp.com/films");
+
+ghibli
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // console.log(data)
+    // let list = document.querySelector('.list')
+    let container = document.querySelector('.container')
+    data.forEach(movie => {
+      console.log(movie.title)
+      console.log(movie.description)
+      console.log(movie.director)
+      console.log(movie.release_date)
+      console.log(movie.rt_score + "%")
+      let html = `
+      <div>
+      <div class="card">${movie.title}</div>
+      <h2>${movie.description}</h2>
+      <h3>${movie.director}</h3>
+      </div?
+      `
+      container.insertAdjacentHTML('beforeend', html)
+    });
+  })
+
+// getAPI();
+
+
+// <%# <% data.data.forEach(element => { %>
+//   <%# <div class="col-sm-4">
+//     <div class="card">
+//       <div class="card-body">
+//         <h5 class="card-title"><%= element.title %>
+//         <%# <h6><%= element.release_date %>
+//         <%# <p class = "card-text"><%= element.description %>
+//         <%# <h6><%= element.rt_score %>
+//         <%# <a href="" class="btn btn-primary">See More</%#> %>
+//       <%# </div> %>
+//     <%# </div> %>
+//   <%# </div> %>
+// <%# <% }); %> 
